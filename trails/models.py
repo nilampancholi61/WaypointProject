8542b2f1eeac901f6547132c2aab48390067b613
nilampancholi61 <155118869+nilampancholi61@users.noreply.github.com>
2026-08-14 11:32:52 -0400

    week 13: add Park model

--- trails/models.py
+++ trails/models.py
@@ -9,6 +9,13 @@ class Trail(models.Model):
         ("expert", "Expert"),
     ]
 
+    class Park(models.Model):
+        name = models.CharField(max_length=200)
+        region = models.CharField(max_length=200)
+
+        def __str__(self):
+            return self.name
+
     name = models.CharField(max_length=200)
     distance_km = models.DecimalField(max_digits=6, decimal_places=2)
     elevation_gain = models.IntegerField()
