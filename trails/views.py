2b93e80686f1bcd8e7e466ac97369d57fd6de1d1
nilampancholi61 <155118869+nilampancholi61@users.noreply.github.com>
2026-08-16 07:05:31 -0400

    week 13: filter trails by park

--- trails/views.py
+++ trails/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
 
-from .models import Trail
+from .models import Park, Trail
 
 
 def catalog(request):
-    trails = Trail.objects.filter(is_open=True).order_by("distance_km")
+    park_id = request.GET.get("park")
+
+    trails = Trail.objects.filter(
+        is_open=True
+    ).order_by("distance_km")
+
+    selected_park = None
+
+    if park_id:
+        selected_park = Park.objects.get(id=park_id)
+        trails = trails.filter(park=selected_park)
 
     return render(
         request,
         "catalog.html",
-        {"trails": trails},
+        {
+            "trails": trails,
+            "parks": Park.objects.all().order_by("name"),
+            "selected_park": selected_park,
+        },
     )
